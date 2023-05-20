a7d47102d9232d886933cbc28aa208f8ab7b80a7
Gawain Bracy II <bracy.gawain2014@gmail.com>
2023-05-20 13:04:55 -0400

    bug fix
    Only show videos with “Ready” status.

--- Creative/apps/creative-tv/src/components/AllAssets.tsx
+++ Creative/apps/creative-tv/src/components/AllAssets.tsx
@@ -30,133 +30,135 @@ export default function AllAssets() {
     return <Box children='error' />
   }
 
+  const readyVideos = videos.data.filter((video) => video.status.phase === 'ready')
+
   return (
-    <LivepeerConfig client={useLivepeerClient}>
-        <SimpleGrid spacing={4} minChildWidth={350}>
-          {videos.data.map((video) => (
-            <Card
-             key={video.id}
-             maxW='md'
-             variant={"elevated"}>
-              <CardHeader>
-                <Flex>
-                  <Flex flex={1} gap={4} align="center" flexWrap={'wrap'}>
-                    <Avatar name='name' src={SITE_LOGO} />
-                    <Box>
-                      <Heading size='sm'>thecreative.eth</Heading>
-                      <Text>Creator</Text>
-                    </Box>
-                  </Flex>
+  <LivepeerConfig client={useLivepeerClient}>
+      <SimpleGrid spacing={4} minChildWidth={350}>
+        {readyVideos.map((video) => (
+          <Card
+            key={video.id}
+            maxW='md'
+            variant={"elevated"}>
+            <CardHeader>
+              <Flex>
+                <Flex flex={1} gap={4} align="center" flexWrap={'wrap'}>
+                  <Avatar name='name' src={SITE_LOGO} />
+                  <Box>
+                    <Heading size='sm'>thecreative.eth</Heading>
+                    <Text>Creator</Text>
+                  </Box>
                 </Flex>
-                </CardHeader>
-                <>
-              <Player 
-                  title={video.name}
-                  playbackId={video.playbackId}
-                  showTitle
-                  poster={<PosterImage />}
-                  showLoadingSpinner
-                  controls={{ autohide: 500, hotkeys: false }}
-                  autoPlay
-                  muted
-                  aspectRatio='16to9'
-                  showPipButton
-                  autoUrlUpload={{fallback: true, ipfsGateway: 'https://w3s.link'}}
-                  theme={{
-                    borderStyles: {
-                      containerBorderStyle: 'solid',
-                    },
-                    colors: {
-                      accent: '#EC407A',
-                    },
-                    space: {
-                      controlsBottomMarginX: '10px',
-                      controlsBottomMarginY: '5px',
-                      controlsTopMarginX: '15px',
-                      controlsTopMarginY: '10px',
-                    },
-                    radii: {
-                      containerBorderRadius: '0px',
-                    },
-                  }}
-                  />
-              </>
-                <CardBody>
-                  <Badge colorScheme={video.status.phase === 'ready' ? 'green' : 'red'}>{video.status.phase}</Badge>
-                  <Stack mt="6" spacing="3">
-                    <Heading size={'md'}>{video.name.slice(0, -4)}</Heading>
-                    <Text>
-                      With Creative TV, I wanted to sync the speed of creation with the speed
-                      of design. I wanted the creator to be just as excited as the designer to
-                      create new content.
-                    </Text>
-                  </Stack>
-              </CardBody>
-              <Divider />
-              <CardFooter
-                justify='space-between'
-                flexWrap='wrap'
-                sx={{
-                  '& > button': {
-                    minW: '136px',
+              </Flex>
+              </CardHeader>
+              <>
+            <Player 
+                title={video.name}
+                playbackId={video.playbackId}
+                showTitle
+                poster={<PosterImage />}
+                showLoadingSpinner
+                controls={{ autohide: 500, hotkeys: false }}
+                autoPlay
+                muted
+                aspectRatio='16to9'
+                showPipButton
+                autoUrlUpload={{fallback: true, ipfsGateway: 'https://w3s.link'}}
+                theme={{
+                  borderStyles: {
+                    containerBorderStyle: 'solid',
+                  },
+                  colors: {
+                    accent: '#EC407A',
+                  },
+                  space: {
+                    controlsBottomMarginX: '10px',
+                    controlsBottomMarginY: '5px',
+                    controlsTopMarginX: '15px',
+                    controlsTopMarginY: '10px',
+                  },
+                  radii: {
+                    containerBorderRadius: '0px',
                   },
                 }}
-              >
-              {video.status.phase === 'ready' ? (
-                <>
-                <Button flex='1' variant='ghost' leftIcon={<ChatIcon />}>
+                />
+            </>
+              <CardBody>
+                <Badge colorScheme={video.status.phase === 'ready' ? 'green' : 'red'}>{video.status.phase}</Badge>
+                <Stack mt="6" spacing="3">
+                  <Heading size={'md'}>{video.name.slice(0, -4)}</Heading>
+                  <Text>
+                    With Creative TV, I wanted to sync the speed of creation with the speed
+                    of design. I wanted the creator to be just as excited as the designer to
+                    create new content.
+                  </Text>
+                </Stack>
+            </CardBody>
+            <Divider />
+            <CardFooter
+              justify='space-between'
+              flexWrap='wrap'
+              sx={{
+                '& > button': {
+                  minW: '136px',
+                },
+              }}
+            >
+            {video.status.phase === 'ready' ? (
+              <>
+              <Button flex='1' variant='ghost' leftIcon={<ChatIcon />}>
+                Comment
+              </Button>
+              <Button flex='1' variant='ghost' leftIcon={<LinkIcon />}>
+                Share
+              </Button>
+              <Button 
+                backgroundColor={'#EC407A'}
+                onClick={() => router.push(`/mint-nft-video?assetId=${video.id}`)}
+                className="card-mint-button"
+                as={motion.div}
+                _hover={{ transform: 'scale(1.1)', cursor: 'pointer' }}
+                flex='1' 
+                variant='ghost' 
+                leftIcon={<DownloadIcon />}>
+                Collect
+              </Button>
+              </>
+              ) : (
+              <>
+                <Button 
+                  flex='1' 
+                  disabled 
+                  variant='ghost' 
+                  leftIcon={<ChatIcon />}>
                   Comment
                 </Button>
-                <Button flex='1' variant='ghost' leftIcon={<LinkIcon />}>
+                <Button 
+                  flex='1'
+                  disabled 
+                  variant='ghost' 
+                  leftIcon={<LinkIcon />}
+                  >
                   Share
                 </Button>
-                <Button 
+                <Button
+                  leftIcon={<DownloadIcon />}
                   backgroundColor={'#EC407A'}
-                  onClick={() => router.push(`/mint-nft-video?assetId=${video.id}`)}
+                  disabled 
                   className="card-mint-button"
                   as={motion.div}
                   _hover={{ transform: 'scale(1.1)', cursor: 'pointer' }}
                   flex='1' 
                   variant='ghost' 
-                  leftIcon={<DownloadIcon />}>
+                >
                   Collect
                 </Button>
-                </>
-                ) : (
-                <>
-                  <Button 
-                    flex='1' 
-                    disabled 
-                    variant='ghost' 
-                    leftIcon={<ChatIcon />}>
-                    Comment
-                  </Button>
-                  <Button 
-                    flex='1'
-                    disabled 
-                    variant='ghost' 
-                    leftIcon={<LinkIcon />}
-                    >
-                    Share
-                  </Button>
-                  <Button
-                    leftIcon={<DownloadIcon />}
-                    backgroundColor={'#EC407A'}
-                    disabled 
-                    className="card-mint-button"
-                    as={motion.div}
-                    _hover={{ transform: 'scale(1.1)', cursor: 'pointer' }}
-                    flex='1' 
-                    variant='ghost' 
-                  >
-                    Collect
-                  </Button>
-                </>
-                )}
-              </CardFooter>
-            </Card>
-          ))}
-        </SimpleGrid>
-    </LivepeerConfig>
+              </>
+              )}
+            </CardFooter>
+          </Card>
+        ))}
+      </SimpleGrid>
+  </LivepeerConfig>
   )
 }
