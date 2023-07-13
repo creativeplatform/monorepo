a5fe6179660859d00ba39fd9d3f51593ef71a3c1
Gawain Bracy II <bracy.gawain2014@gmail.com>
2023-07-13 19:29:46 -0400

    Updated Paywall
    Included Unlock paywall.
    
    *Todo update wallet UI

--- Creative/apps/creative-tv/src/pages/_document.tsx
+++ Creative/apps/creative-tv/src/pages/_document.tsx
@@ -8,6 +8,32 @@ export default function Document() {
       <Head>
         <meta charSet="utf-8" />
         <link rel="icon" href="/favicon.ico" />
+        <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(d, s) {
+                var js = d.createElement(s),
+                  sc = d.getElementsByTagName(s)[0];
+                js.src = "https://paywall.unlock-protocol.com/static/unlock.latest.min.js";
+                sc.parentNode.insertBefore(js, sc);
+              }(document, "script"));`,
+            }}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              var unlockProtocolConfig = {
+                "locks": {
+                  "0x697560ba635e92c19e660fa0eb0bdfcd7938a08b": {
+                    "name": "Member Test",
+                    "network": 5,
+                  }
+                },    
+                "title": "Creative TV Membership",
+                "icon": "https://bafkreiehm3yedt4cmtckelgfwqtgfvp6bolvk5nx2esle4tnwe7mi5q43q.ipfs.nftstorage.link/"           
+              }`,
+            }}
+          />
       </Head>
       <body>
         <ColorModeScript initialColorMode={THEME_INITIAL_COLOR} />
