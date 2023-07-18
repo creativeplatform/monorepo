06b3eb4a1ae644169babdcd7543b757ad2ee6aa8
sky cherfa <sherifcherfa@gmail.com>
2023-07-18 06:08:14 -0400

    chakraUI && react-hook-form, some work on tx

--- Creative/apps/creative-tv/src/components/MeTokenCreationForm.tsx
+++ Creative/apps/creative-tv/src/components/MeTokenCreationForm.tsx
@@ -1,61 +1,156 @@
+import React, { useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
 import { useAddress } from '@thirdweb-dev/react';
-import React, { useState } from 'react'
 import { createMeToken } from 'utils/fetchers/createMeToken';
+import { getMeTokenContract } from 'utils/fetchers/createMeToken';
+import { Box, Button, FormControl, FormErrorMessage, FormLabel, Heading, Input, Stack } from '@chakra-ui/react';
 
 export default function MeTokenCreationForm() {
-    const [name, setName] = useState("");
-    const [symbol, setSymbol] = useState("");
-    const [hubId, setHubId] = useState(0);
-    const [assetsDeposited, setAssetsDeposited] = useState("");
-    const address = useAddress();
+  const { register, handleSubmit, formState: { errors }, reset } = useForm();
+  const address = useAddress();
+  const [meTokenContract, setMeTokenContract] = useState<any>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-    const handleSubmit = async (e: any) => {
-        e.preventDefault();
-        console.log({ name, symbol, hubId, assetsDeposited });
-        const data = await createMeToken({name, symbol, hubId, assetsDeposited}, address!);
-        // do something with the form data
+  useEffect(() => {
+    const getContract = async () => {
+      const contract = await getMeTokenContract(address || '');
+      setMeTokenContract(contract);
     };
+    getContract();
+  }, [address]);
 
-    return (
-      <>
+  console.log(meTokenContract);
 
-        <form onSubmit={handleSubmit} style={{ border: '2px solid black', height: '400px', display: 'flex', flexDirection: 'column', padding: '1em', backgroundColor: 'lightgray' }}>
-        <h1 style={{ marginBottom: '1em', fontSize: 'xl' }}>MeToken Creation Form</h1>
-            <label>
-                Name:
-                <input
-                    type="text"
-                    value={name}
-                    onChange={e => setName(e.target.value)}
-                />
-            </label>
-            <label>
-                Symbol:
-                <input
-                    type="text"
-                    value={symbol}
-                    onChange={e => setSymbol(e.target.value)}
-                />
-            </label>
-            <label>
-                Hub ID:
-                <input
-                    type="number"
-                    value={hubId}
-                    onChange={e => setHubId(parseInt(e.target.value, 10))}
-                />
-            </label>
-            <label>
-                Assets Deposited:
-                <input
-                    type="text"
-                    value={assetsDeposited}
-                    onChange={e => setAssetsDeposited(e.target.value)}
-                />
-            </label>
-            <input type="submit" value="Submit" />
-        </form>
-      </>
-        
-    );
+  const onSubmit = async (data: any) => {
+    setIsLoading(true);
+
+    try {
+      const { name, symbol, hubId, assetsDeposited } = data;
+      const tx = await createMeToken({ name, symbol, hubId, assetsDeposited }, meTokenContract);
+      console.log(tx);
+      setIsSubmitted(true);
+      reset();
+    } catch (error) {
+      console.log('Error:', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    let timer: any;
+
+    if (isSubmitted) {
+      timer = setTimeout(() => {
+        setIsSubmitted(false);
+        setIsLoading(false);
+      }, 3000);
+    }
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [isSubmitted]);
+
+  const inputBoxStyle = {
+    borderRadius: '4px',
+    display: 'flex',
+    width: '45vw',
+    marginBottom: '15px',
+    padding: '15px',
+  };
+
+  return (
+    <>
+      <Box
+        as="form"
+        onSubmit={handleSubmit(onSubmit)}
+        bg="#171923"
+        border="4px solid #EDEDEE"
+        borderRadius="15px"
+        width="100%"
+        display="flex"
+        flexDirection="column"
+        padding="1rem"
+        alignItems="center"
+        marginBottom="30px"
+      >
+        <Heading marginBottom="0.5em" fontSize="2em" fontWeight="bold" color="#EDEDEE">
+          MeToken Creation Form
+        </Heading>
+        <hr width="85%" margin="0 auto" marginBottom="3em" border="none" borderBottom="1px solid #EDEDEE" />
+        <Stack spacing={4} width="45vw">
+          <FormControl isInvalid={!!errors.name}>
+            <FormLabel color="#EDEDEE">Name:</FormLabel>
+            <Input
+              type="text"
+              placeholder="Your meToken Name"
+              {...register('name', { required: true })}
+            />
+            <FormErrorMessage>This field is required</FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={!!errors.symbol}>
+            <FormLabel color="#EDEDEE">Symbol:</FormLabel>
+            <Input
+              type="text"
+              placeholder="Your meToken symbol"
+              {...register('symbol', { required: true })}
+            />
+            <FormErrorMessage>This field is required</FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={!!errors.hubId}>
+            <FormLabel color="#EDEDEE">Hub ID:</FormLabel>
+            <Input
+              type="number"
+              placeholder="Your Hub ID number"
+              {...register('hubId', { required: true })}
+            />
+            <FormErrorMessage>This field is required</FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={!!errors.assetsDeposited}>
+            <FormLabel color="#EDEDEE">Assets Deposited:</FormLabel>
+            <Input
+              type="text"
+              placeholder="Number of assets deposited"
+              {...register('assetsDeposited', { required: true })}
+            />
+            <FormErrorMessage>This field is required</FormErrorMessage>
+          </FormControl>
+        </Stack>
+        {isLoading ? (
+          <Button
+            type="submit"
+            disabled
+            background="linear-gradient(to right, #E03C88, #E34335, #F6B138)"
+            borderRadius="10px"
+            fontSize="18px"
+            fontWeight="bold"
+            height="50px"
+            width="75%"
+            padding="10px"
+            marginTop="20px"
+            marginBottom="40px"
+          >
+            Creating meToken...
+          </Button>
+        ) : (
+          <Button
+            type="submit"
+            background="linear-gradient(to right, #E03C88, #E34335, #F6B138)"
+            borderRadius="10px"
+            fontSize="18px"
+            fontWeight="bold"
+            height="50px"
+            width="75%"
+            padding="10px"
+            marginTop="20px"
+            marginBottom="40px"
+          >
+            {isSubmitted ? 'meToken Created!' : 'Submit'}
+          </Button>
+        )}
+      </Box>
+    </>
+  );
 }
--- Creative/apps/creative-tv/src/utils/fetchers/createMeToken.ts
+++ Creative/apps/creative-tv/src/utils/fetchers/createMeToken.ts
@@ -10,22 +10,68 @@ interface CreateMeToken {
   name: string;
   symbol: string;
   hubId: number;
-  assetsDeposited: string;
+  assetsDeposited: number;
 }
 
-
+export const daiAddress = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
 export const meTokenRegistryFacet = `${METOKENS_ADDRESS_GOERLI.meTokensRegistryFacet}`
 
-export const createMeToken = async (createMeTokenData: CreateMeToken, address: string) => {
+export const getMeTokenContract = async (address: string) => {
   const signer = new ethers.providers.JsonRpcProvider('https://ethereum-goerli.publicnode.com').getSigner(address)
 
-// Create an SDK signer using the Thirdweb SDK
-const sdkSigner = await ThirdwebSDK.fromSigner(signer, Goerli)
-const meTokenRegistryFacetContract = await sdkSigner.getContract(meTokenRegistryFacet, METOKENS_REGISTRY_ABI)
-  const { name, symbol, hubId, assetsDeposited } = createMeTokenData;
-  console.log(meTokenRegistryFacetContract, 'test')
-  return createMeTokenData;
-  /* const tx = await factoryContract.subscribe(name, symbol, hubId, assetsDeposited);
-
-  return tx; */
+  // Create an SDK signer using the Thirdweb SDK
+  const sdkSigner = await ThirdwebSDK.fromSigner(signer, Goerli)
+  const meTokenRegistryFacetContract = await sdkSigner.getContract(meTokenRegistryFacet, METOKENS_REGISTRY_ABI)
+  return meTokenRegistryFacetContract;
 }
+
+export const createMeToken = async (createMeTokenData: CreateMeToken, contract: any) => {
+  
+  try {
+    const data = await contract.call(
+        "subscribe", // Name of your function as it is on the smart contract
+        // Arguments to your function, in the same order they are on your smart contract
+        [
+          createMeTokenData.name, // e.g. Argument 1
+          createMeTokenData.symbol, // e.g. Argument 2
+          createMeTokenData.hubId,
+          createMeTokenData.assetsDeposited,
+        ],
+      );
+    console.log(data);
+    // do something with the form data  
+  } catch (error) {
+    console.log('Error:', error);
+  }
+}
+
+/* export const isApproved = async (
+  tokenAddress: string,
+  amount: number,
+  owner: string,
+  address: string,
+) => {
+  const signer = new ethers.providers.JsonRpcProvider('https://ethereum-goerli.publicnode.com').getSigner(address)
+  const provider  = new ethers.providers.JsonRpcProvider('https://ethereum-goerli.publicnode.com')
+
+  const erc20 = await new Contract(
+    tokenAddress,
+    erc20ABI,
+    mainnetProvider.getSigner(owner),
+  );
+  const approvalAmount = await erc20.allowance(owner, foundryFacet);
+  return approvalAmount.gt(amount);
+};
+
+export const approveTokens = async (
+  tokenAddress: string,
+  amount: BigNumber,
+  provider: JsonRpcProvider,
+): Promise<TransactionResponse> => {
+  const erc20 = await new Contract(
+    tokenAddress,
+    erc20ABI,
+    provider.getSigner(),
+  );
+  return erc20.approve(foundryFacet, amount);
+}; */
