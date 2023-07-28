0f45100e725ae34ce4a51c14fa06e572c756ef1c
sky cherfa <sherifcherfa@gmail.com>
2023-07-28 07:39:16 -0400

    fix

--- Creative/apps/creative-tv/src/components/MeTokenCreationForm.tsx
+++ Creative/apps/creative-tv/src/components/MeTokenCreationForm.tsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { useForm } from 'react-hook-form'
-import { ConnectWallet, useAddress, useContract, useContractRead, useContractWrite, useDisconnect, useSDK, useSigner } from '@thirdweb-dev/react'
-import { createMeToken, approveTokens, isApprovedAmount } from 'utils/fetchers/createMeToken'
+import { ConnectWallet, useAddress, useSigner } from '@thirdweb-dev/react'
+import { createMeToken, approveTokens, isApprovedAmount, getMeTokenInfo } from 'utils/fetchers/createMeToken'
 import { getMeTokenContract } from 'utils/fetchers/createMeToken'
 import { Box, Button, Divider, FormControl, FormErrorMessage, FormLabel, Heading, Input, Stack } from '@chakra-ui/react'
+import { getMeTokenFor } from 'utils/fetchers/createMeToken'
+import Image from 'next/image'
+import { useRouter } from 'next/router'
 
 export default function MeTokenCreationForm() {
   const {
@@ -19,8 +22,35 @@ export default function MeTokenCreationForm() {
   const [isSubmitted, setIsSubmitted] = useState(false)
   const [isApproved, setApproved] = useState(false)
   const [approvalAmount, setApprovalAmount] = useState(0)
+  const [meTokenAddress, setMeTokenAddress] = useState('')
+  const [meTokenInfo, setMeTokenInfo] = useState<any>(null)
   const signer = useSigner()
+  const router = useRouter()
+  console.log(router.query, 'router.query')
 
+  useEffect(() => {
+    const addy = router.query.address || address
+    console.log(addy)
+    if (!addy || !signer) return
+    const getMeToken = async () => {
+      console.log(addy, signer)
+      
+      getMeTokenFor((addy as string), signer).then((res) => {
+        console.log(res, 'res')
+        setMeTokenAddress(res)
+      })
+    }
+    getMeToken()
+  }, [router.query, address, signer])
+
+  useEffect(() => {
+    if (!meTokenAddress || !address || !signer) return
+    const getData = async () => {
+      getMeTokenInfo(meTokenAddress, address, signer).then(setMeTokenInfo)
+    }
+    getData()
+  }, [meTokenAddress, signer, address])
+  console.log(meTokenAddress)
   useEffect(() => {
     const getContract = async () => {
       const contract = await getMeTokenContract(signer)
@@ -30,13 +60,12 @@ export default function MeTokenCreationForm() {
   }, [address])
 
   useEffect(() => {
-    if (address && signer) {
-      const amount = isApprovedAmount(address, signer)
-      console.log(amount)
+    const assetsDeposited = getValues('assetsDeposited')
+    if (address && signer && assetsDeposited) {
+      const amount = isApprovedAmount(address, assetsDeposited, signer)
+      amount.then(setApprovalAmount)
     }
   }, [address, signer])
-
-  console.log({ meTokenContract })
 
   const approve = async () => {
     const assetsDeposited = getValues('assetsDeposited')
@@ -78,12 +107,15 @@ export default function MeTokenCreationForm() {
     }
   }, [isSubmitted])
 
-  const inputBoxStyle = {
-    borderRadius: '4px',
-    display: 'flex',
-    width: '45vw',
-    marginBottom: '15px',
-    padding: '15px',
+  const approveBuy = async () => {
+    console.log('approving')
+    const purchaseAmount = getValues('purchaseAmount')
+    await approveTokens(purchaseAmount, signer)
+    setApproved(true)
+  }
+
+  const buy = async () => {
+    console.log('buying')
   }
 
   return (
@@ -101,41 +133,23 @@ export default function MeTokenCreationForm() {
       padding="1px"
       alignItems="center"
       mb="30px">
-      <Box bg="#171923" position="relative" zIndex={2} p={8} overflow="hidden" width="100%" height="100%" borderRadius="3xl">
-        <Heading mb={3} fontSize="2em" fontWeight="bold" color="#EDEDEE" textAlign="center">
-          meToken Creation Form
-        </Heading>
-        <Divider width="75%" margin="0 auto" marginBottom="3em" border="none" borderBottom="1px solid #EDEDEE" />
-        <Stack spacing={4} width="100%">
-          <FormControl isInvalid={!!errors.name}>
-            <FormLabel color="white">Name:</FormLabel>
-            <Input type="text" placeholder="Your meToken Name" {...register('name', { required: true })} />
-            <FormErrorMessage>This field is required</FormErrorMessage>
-          </FormControl>
-          <FormControl isInvalid={!!errors.symbol}>
-            <FormLabel color="#EDEDEE">Symbol:</FormLabel>
-            <Input type="text" placeholder="Your meToken symbol" {...register('symbol', { required: true })} />
-            <FormErrorMessage>This field is required</FormErrorMessage>
-          </FormControl>
-
-            <Input type="hidden" value="1" {...register('hubId')} />
-          <FormControl isInvalid={!!errors.assetsDeposited}>
-            <FormLabel color="#EDEDEE">Assets Deposited:</FormLabel>
-            <Input type="text" placeholder="Number of assets deposited" {...register('assetsDeposited', { required: true })} />
-            <FormErrorMessage>This field is required</FormErrorMessage>
-          </FormControl>
-        </Stack>
-        <Box display="flex" flexDirection="row" alignItems="center" justifyContent="start" mt={5} gap={5} sx={{
-          button: {
-            minWidth: '150px',
-            borderRadius: "10px",
-            fontSize: "18px",
-            fontWeight:"bold",
-            height:"50px",
-            padding:"10px"
-          }
-        }}>
-          <Button
+      {
+        meTokenInfo ? (
+          <Box bg="#171923" position="relative" zIndex={2} p={8} overflow="hidden" width="100%" height="100%" borderRadius="3xl">
+          <Heading mb={3} fontSize="2em" fontWeight="bold" color="#EDEDEE" textAlign="center">
+            meToken Info
+          </Heading>
+          <Divider width="75%" margin="0 auto" marginBottom="3em" border="none" borderBottom="1px solid #EDEDEE" />
+          <Stack spacing={4} width="100%">
+            <Box>{meTokenInfo.meTokenAddress}</Box>
+            <Box>{meTokenInfo.symbol}</Box>
+            <Image src={meTokenInfo.image} alt={''} style={{ height: '100px', width: '100px' }} />
+            <FormControl isInvalid={!!errors.name}>
+              <FormLabel color="white">Amount to purchase (DAI):</FormLabel>
+              <Input type="text" placeholder="Amount To Purchase" {...register('purchaseAmount', { required: true })} />
+              <FormErrorMessage>This field is required</FormErrorMessage>
+            </FormControl>
+            <Button
             background="linear-gradient(to right, #E03C88, #E34335, #F6B138)"
             onClick={approve}
             variant="solid"
@@ -143,23 +157,79 @@ export default function MeTokenCreationForm() {
             >
               Approve
             </Button>
-        {isLoading && isApproved ? (
-          <Button
-            type="submit"
-            disabled
-            background="linear-gradient(to right, #E03C88, #E34335, #F6B138)">
-            Creating meToken...
-          </Button>
+            <Button
+            background="linear-gradient(to right, #E03C88, #E34335, #F6B138)"
+            onClick={buy}
+            variant="solid"
+            disabled={isApproved}
+            >
+              Buy
+            </Button>
+          </Stack>
+          </Box>
         ) : (
-          <Button
-            disabled={!isApproved || isLoading}
-            type="submit"
-            background="linear-gradient(to right, #E03C88, #E34335, #F6B138)">
-            {isSubmitted ? 'meToken Created!' : 'Submit'}
-          </Button>
-          )}
-        </Box>        
-      </Box>
+          <Box bg="#171923" position="relative" zIndex={2} p={8} overflow="hidden" width="100%" height="100%" borderRadius="3xl">
+          <Heading mb={3} fontSize="2em" fontWeight="bold" color="#EDEDEE" textAlign="center">
+            meToken Creation Form
+          </Heading>
+          <Divider width="75%" margin="0 auto" marginBottom="3em" border="none" borderBottom="1px solid #EDEDEE" />
+          <Stack spacing={4} width="100%">
+            <FormControl isInvalid={!!errors.name}>
+              <FormLabel color="white">Name:</FormLabel>
+              <Input type="text" placeholder="Your meToken Name" {...register('name', { required: true })} />
+              <FormErrorMessage>This field is required</FormErrorMessage>
+            </FormControl>
+            <FormControl isInvalid={!!errors.symbol}>
+              <FormLabel color="#EDEDEE">Symbol:</FormLabel>
+              <Input type="text" placeholder="Your meToken symbol" {...register('symbol', { required: true })} />
+              <FormErrorMessage>This field is required</FormErrorMessage>
+            </FormControl>
+  
+              <Input type="hidden" value="1" {...register('hubId')} />
+            <FormControl isInvalid={!!errors.assetsDeposited}>
+              <FormLabel color="#EDEDEE">Assets Deposited:</FormLabel>
+              <Input type="text" placeholder="Number of assets deposited" {...register('assetsDeposited', { required: true })} />
+              <FormErrorMessage>This field is required</FormErrorMessage>
+            </FormControl>
+          </Stack>
+          <Box display="flex" flexDirection="row" alignItems="center" justifyContent="start" mt={5} gap={5} sx={{
+            button: {
+              minWidth: '150px',
+              borderRadius: "10px",
+              fontSize: "18px",
+              fontWeight:"bold",
+              height:"50px",
+              padding:"10px"
+            }
+          }}>
+            <Button
+              background="linear-gradient(to right, #E03C88, #E34335, #F6B138)"
+              onClick={approve}
+              variant="solid"
+              disabled={isApproved}
+              >
+                Approve
+              </Button>
+          {isLoading && isApproved ? (
+            <Button
+              type="submit"
+              disabled
+              background="linear-gradient(to right, #E03C88, #E34335, #F6B138)">
+              Creating meToken...
+            </Button>
+          ) : (
+            <Button
+              disabled={!isApproved || isLoading}
+              type="submit"
+              background="linear-gradient(to right, #E03C88, #E34335, #F6B138)">
+              {isSubmitted ? 'meToken Created!' : 'Submit'}
+            </Button>
+            )}
+          </Box>        
+        </Box>
+        )
+      }
+     
       <Box position="absolute" inset="-1px" borderRadius="3xl" background="linear-gradient(to top, #E03C88, #E34335, #F6B138, transparent, transparent)" zIndex={0} />
     </Box>
   )
--- Creative/apps/creative-tv/src/utils/fetchers/createMeToken.ts
+++ Creative/apps/creative-tv/src/utils/fetchers/createMeToken.ts
@@ -1,13 +1,14 @@
 import { ethers,  Contract } from 'ethers'
 import { ThirdwebSDK } from '@thirdweb-dev/sdk';
 import { Goerli } from '@thirdweb-dev/chains';
+import { Orbis } from '@orbisclub/orbis-sdk';
 import {
   METOKENS_ADDRESS_GOERLI,
   METOKENS_REGISTRY_ABI,
   ERC20_ABI,
-  GOERLI_PROVIDER_URL
+  HUB_FACET_ABI,
+  FOUNDRY_FACET_ABI,
 } from '../config'
-import { ExternalProvider, Web3Provider } from '@ethersproject/providers';
 
 interface CreateMeToken {
   name: string;
@@ -19,6 +20,8 @@ export const foundryFacet = `${METOKENS_ADDRESS_GOERLI.foundryFacet}`
 export const daiAddress = '0x0c4f55df47b20c8fbb134494b45a7a01097849ff';
 export const meTokenRegistryFacet = `${METOKENS_ADDRESS_GOERLI.meTokensRegistryFacet}`
 export const meTokenDiamond = `${METOKENS_ADDRESS_GOERLI.meTokenDiamond}`
+export const meTokenApproval = '0x47fF07BfD0cdFaD6650177f661E8cC977FA8Bf36'
+
 
 export const getMeTokenContract = async (signer: any) => {
   const sdkSigner = await ThirdwebSDK.fromSigner(signer, Goerli);
@@ -56,23 +59,78 @@ export const createMeToken = async (createMeTokenData: CreateMeToken, contract: 
   return metoken.subscribe(createMeTokenData.name, // e.g. Argument 1
   createMeTokenData.symbol, // e.g. Argument 2
   +createMeTokenData.hubId,
-  100)
+  ethers.utils.parseEther(createMeTokenData.assetsDeposited.toString()))
 }
+
+export const getMeTokenFor = async (ownerAddress: string, signer: any) => {
+  console.log(ownerAddress, 'ownerAddress')
+  if (!ownerAddress) throw new Error('Owner required for `getMeTokenFor`.');
+  console.log('made it in')
+  const registry = await new Contract(
+    meTokenDiamond,
+    METOKENS_REGISTRY_ABI,
+    signer,
+  );
+  const addressChecksummed = ethers.utils.getAddress(ownerAddress);
+  console.log(registry, 'registry')
+  const token =  registry.getOwnerMeToken(addressChecksummed);
+  console.log(token, 'token')
+  return token
+};
+
+export const mint = (
+  meToken: string,
+  amount: string,
+  recipient: string,
+  signer: any
+) => {
+  const meTokenFoundry = new Contract(
+    meTokenDiamond,
+    FOUNDRY_FACET_ABI,
+    signer,
+  );
+  return meTokenFoundry.mint(meToken, amount, recipient);
+};
+
+export const getMeTokenInfo = async (tokenAddress: string, owner: string, signer: any) => {
+  if (!tokenAddress || !owner || !signer)
+    throw new Error('Token address required for `getMeTokenInfo`.');
+  console.log(tokenAddress, 'tokenAddress')
+  const orbis = new Orbis();
+  const { data: dids } = await orbis.getDids(owner);
+
+  const erc20 = await new Contract(tokenAddress, ERC20_ABI, signer);
+  const registry = await new Contract(
+    meTokenDiamond,
+    METOKENS_REGISTRY_ABI,
+    signer,
+  );
+  const hub = new Contract(meTokenDiamond, HUB_FACET_ABI, signer);
+  const tokenInfo = await registry.getMeTokenInfo(tokenAddress);
+    console.log(dids[0].details?.profile?.pfp, 'dids')
+  return {
+    symbol: `$${await erc20.symbol()}`,
+    profilePicture:
+    dids[0].details?.profile?.pfp || 'https://tinyurl.com/mr29vhmk',
+    tokenAddress,
+    collateralAddress: await hub
+      .getBasicHubInfo(tokenInfo?.hubId)
+      .then(({ asset }: { asset: string }) => asset),
+  };
+};
 
 export const isApprovedAmount = async (
   address: string,
+  amount: string,
   signer: any,
 ) => {
-  const sdkSigner = await ThirdwebSDK.fromSigner(signer, Goerli)
-  console.log(sdkSigner, 'sdkSigner')
-  const erc20 = await sdkSigner.getContractFromAbi(
+  const erc20 = await new Contract(
     daiAddress,
     ERC20_ABI,
+    signer,
   );
-  return await erc20.call(
-    'allowance',
-    [meTokenDiamond, address],
-  );
+  const approvalAmount = await erc20.allowance(address, foundryFacet);
+  return approvalAmount.gt(amount);
 };
 
 /* export const approveTokens = async (
@@ -117,6 +175,6 @@ export const approveTokens = async (
     ERC20_ABI,
     s,
   );
-  return erc20.approve('0x47fF07BfD0cdFaD6650177f661E8cC977FA8Bf36', `${100 * 10 ** 18}`);
+  return erc20.approve(meTokenApproval, ethers.utils.formatEther(amount));
 };
 
