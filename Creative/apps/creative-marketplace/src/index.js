3341ed159f1eaa7f795c2be95b5f6ed317995195
G2 <1490181+sirgawain0x@users.noreply.github.com>
2023-08-30 10:19:23 -0400

    wallet and NFT display update

--- Creative/apps/creative-marketplace/src/index.js
+++ Creative/apps/creative-marketplace/src/index.js
@@ -4,15 +4,37 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import './App.scss'
 import ScrollToTop from "./ScrollToTop";
-
+import { ThirdwebProvider, smartWallet, metamaskWallet, coinbaseWallet, localWallet, walletConnect, paperWallet } from "@thirdweb-dev/react";
+import { ACCOUNT_FACTORY_TESTNET } from "./const/config";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.Fragment>
-        <BrowserRouter>
-            <ScrollToTop />
-            <App />
-        </BrowserRouter>
+        <ThirdwebProvider 
+        activeChain="mumbai" 
+        clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID} 
+        supportedWallets={[
+            smartWallet({
+                factoryAddress: ACCOUNT_FACTORY_TESTNET,
+                gasless: false,
+                personalWallets: [
+                    paperWallet({
+                        paperClientId: process.env.NEXT_PUBLIC_PAPER_CLIENT_ID
+                    }),
+                  metamaskWallet(),
+                  coinbaseWallet(),
+                  walletConnect({
+                    projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT
+                  }),
+                  localWallet({ persist: true })
+                ]
+            })
+        ]}>
+            <BrowserRouter>
+                <ScrollToTop />
+                <App />
+            </BrowserRouter>
+        </ThirdwebProvider>
     </React.Fragment>
 );
 
