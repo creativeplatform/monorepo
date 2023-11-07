788dd1426d1e3735fed4996724707158fcb607f2
G2 <1490181+sirgawain0x@users.noreply.github.com>
2023-11-07 10:51:54 -0500

    minor fix

--- Creative/apps/creative-tv/src/components/layout/Header.tsx
+++ Creative/apps/creative-tv/src/components/layout/Header.tsx
@@ -453,7 +453,7 @@ export function Header({ className, handleLoading }: Props) {
                 {!isLoggedIn && !subscribed ? (
                   <MenuList>
                     <MenuItem onClick={() => wertWidget.mount()}>
-                    Are you Creative?
+                    Are You Creative?
                     </MenuItem>
                     <MenuDivider />
                     <MenuItem
@@ -678,7 +678,7 @@ export function Header({ className, handleLoading }: Props) {
                   {!isLoggedIn && !subscribed ? (
                   <>
                     <MenuItem onClick={() => wertWidget.mount()}>
-                    Are you Creative?
+                    Are You Creative?
                     </MenuItem>
                       <MenuDivider />
                     </>
