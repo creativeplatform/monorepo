dc40b53b437a29bc232e1eb65159cfee77ffc290
G2 <1490181+sirgawain0x@users.noreply.github.com>
2023-11-15 17:06:40 -0500

    color update
    - updated swap

--- Creative/apps/creative-tv/src/pages/profile/[address]/UniswapWidget.tsx
+++ Creative/apps/creative-tv/src/pages/profile/[address]/UniswapWidget.tsx
@@ -1,17 +1,35 @@
-import { SwapWidget } from '@uniswap/widgets'
+import { darkTheme, lightTheme, Theme, SwapWidget } from '@uniswap/widgets'
 import { useSigner, useSmartWallet } from '@thirdweb-dev/react';
+import { useColorMode } from '@chakra-ui/react';
 import '@uniswap/widgets/fonts.css'
 
 const UniswapWidget = () => {
-    const signer = useSigner();
+  const signer = useSigner();
     // Our own JSON-RPC endpoints.
-    const jsonRpcUrlMap = {
+  const jsonRpcUrlMap = {
     137: ['https://polygon.rpc.thirdweb.com'],
     80001: ['https://mumbai.rpc.thirdweb.com'],
   }
+
+  const { colorMode } = useColorMode()
+
+  const myLightTheme: Theme = {
+    ...lightTheme, // Extend the lightTheme
+    accent: '#EC407A',
+    primary: '#000000',
+    secondary: '#565A69',
+  }
+  
+  const myDarkTheme: Theme = {
+    ...darkTheme, // Extend the darkTheme
+    accent: '#EC407A',
+    primary: '#FFFFFF',
+    secondary: '#888D9B',
+  }
+
   return (
     <>
-    <SwapWidget provider={signer?.connect.prototype} jsonRpcUrlMap={jsonRpcUrlMap} />
+    <SwapWidget theme={colorMode === 'dark' ? myDarkTheme : myLightTheme} provider={signer?.connect.prototype} jsonRpcUrlMap={jsonRpcUrlMap} />
     </>
   );
 }
