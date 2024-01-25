55d527f23a8064dc143f346338a96a5f1ee63387
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-01-25 13:04:24 +0100

    fix: refactor type and added event listener

--- Creative/apps/creative-tv/src/components/ListClaimConditions.tsx
+++ Creative/apps/creative-tv/src/components/ListClaimConditions.tsx
@@ -1,8 +1,22 @@
-import { DeleteIcon, EditIcon } from '@chakra-ui/icons'
-import { Box, Button, ButtonGroup, Flex, HStack, Spacer, Text, VStack } from '@chakra-ui/react'
-import { ClaimCondition, ClaimConditionInput, NFT, SmartContract } from '@thirdweb-dev/sdk'
+import { AddIcon, CloseIcon, DeleteIcon, EditIcon } from '@chakra-ui/icons'
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  Badge,
+  Box,
+  Button,
+  ButtonGroup,
+  HStack,
+  Spacer,
+  Stack,
+  Text,
+  VStack,
+} from '@chakra-ui/react'
+import { ClaimCondition, NFT, SmartContract } from '@thirdweb-dev/sdk'
 import { ethers } from 'ethers'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { parseCurrencyDecimals } from 'utils/helpers'
 import { EditClaimConditions } from './EditClaimConditons'
 
@@ -11,76 +25,179 @@ type ListClaimConditionsProps = {
   claimConditions: ClaimCondition[]
   nftMetadata: Record<string, any>
   nftContract: SmartContract<ethers.BaseContract> | undefined
+  addClaimPhase: boolean
+  setAddClaimPhase: (arg: boolean) => void
 }
 
 export const ListClaimConditions = (props: ListClaimConditionsProps) => {
   const [canEditClaim, setCanEditClaim] = useState(false)
+
+  useEffect(() => {
+    //////////////////////////
+    // listen to the events
+    props.nftContract?.events.listenToAllEvents(async (e) => {
+      if (e.eventName == 'ClaimConditionsUpdated') {
+        /* TODO: Remember to clear form fields after successful tx */
+
+        // setCanEditClaim(false)
+        console.log('ListClaimConditions::eventData ', e.data)
+      }
+    })
+    return () => {
+      props.nftContract?.events.removeAllListeners()
+    }
+  })
 
   const deleteClaimById = async (tokenId: string) => {
     // TODO: stub to delete a claim by id
     console.log('deleteClaimById: ', tokenId)
   }
 
-  const handleClaimConditionUpdateById = async (tokenId: string, idx: number, claimInput: ClaimConditionInput) => {
-    // deletes an existing claimCondition by its id
-    const res = await props.nftContract?.erc1155.claimConditions.update(tokenId, idx, claimInput)
-    console.log(res)
+  const isActiveClaimPhase = (date: Date) => {
+    return new Date().getTime() > new Date(date).getTime()
   }
 
   return (
-    <VStack my={8} alignItems={'flex-start'} style={{ border: '1px solid #b2b2b2', borderRadius: '8px', padding: '24px' }}>
-      {props.claimConditions.length > 0 &&
-        props.claimConditions.map((c, i) => (
-          <Box key={i}>
-            <Flex className="header" direction={'row'}>
-              <VStack alignItems={'flex-start'} spacing={0} mb={4}>
-                <Text fontSize={20}>Claims for token ID: #{props.nft?.metadata.id} </Text>
-                <Text color={'gray.300'}>Any wallet can claim this token</Text>
-              </VStack>
-              <Spacer />
-              <ButtonGroup variant="outline">
-                <Button colorScheme="" variant="ghost" leftIcon={<EditIcon />} onClick={() => setCanEditClaim(!canEditClaim)}>
-                  Edit
-                </Button>
-                <Button colorScheme="red" variant="ghost" leftIcon={<DeleteIcon />} onClick={() => deleteClaimById(props.nft?.metadata.id)}>
-                  Delete
-                </Button>
-              </ButtonGroup>
-            </Flex>
+    <>
+      <Box>
+        <Text style={{ fontSize: '24px' }}>Set Claim Conditions</Text>
+        <Text color={'gray.300'} fontStyle={'italic'}>
+          Condition how your NFTs can be claimed
+        </Text>
+      </Box>
 
-            {!canEditClaim && (
-              <Box className="body" key={c.startTime.toDateString()}>
-                <HStack spacing="24px" style={{ fontSize: '14px' }}>
-                  <Box>
-                    <Text style={{ fontWeight: 600, marginBottom: '12px' }}>Phase Start</Text>
-                    <span>{c.startTime.toDateString()}</span>
+      <VStack my={8} alignItems={'flex-start'} style={{ border: '1px solid #b2b2b2', borderRadius: '8px', padding: '24px' }}>
+        {props.claimConditions && props.claimConditions.length > 0 ? (
+          <>
+            <VStack alignItems={'flex-start'} spacing={0} mb={4}>
+              <Text fontSize={20}>Claims for token ID: #{props.nft?.metadata.id} </Text>
+              <Text color={'gray.300'}>Any wallet can claim this token</Text>
+            </VStack>
+
+            {props.claimConditions.map((c, i) => (
+              <Box key={i} bgColor={'gray.600'} p={4} mb={1} borderRadius={4}>
+                <Stack className="header" direction={'row'} alignItems={'flex-start'}>
+                  {isActiveClaimPhase(c.startTime) && (
+                    <Badge variant="solid" alignContent={'center'} size={'sm'} colorScheme="green">
+                      Active
+                    </Badge>
+                  )}
+
+                  <Spacer />
+                  <ButtonGroup variant="outline">
+                    <Button
+                      colorScheme=""
+                      variant="ghost"
+                      leftIcon={canEditClaim ? <CloseIcon boxSize={3} /> : <EditIcon boxSize={3} />}
+                      onClick={() => setCanEditClaim(!canEditClaim)}>
+                      {canEditClaim ? 'Cancel Edit' : 'Edit'}
+                    </Button>
+                    <Button
+                      colorScheme="red"
+                      variant="ghost"
+                      leftIcon={<DeleteIcon boxSize={3} />}
+                      onClick={() => deleteClaimById(props.nft?.metadata.id)}>
+                      Delete
+                    </Button>
+                  </ButtonGroup>
+                </Stack>
+
+                {canEditClaim ? (
+                  <EditClaimConditions
+                    nftContract={props.nftContract}
+                    nft={props.nft}
+                    ccIndex={i}
+                    claimCondition={props.claimConditions[i]}
+                    setCanEditClaim={setCanEditClaim}
+                  />
+                ) : (
+                  <Box className="body" key={c.startTime.toDateString()}>
+                    <HStack spacing="24px" style={{ fontSize: '14px' }}>
+                      <Box>
+                        <Text style={{ fontWeight: 600, marginBottom: '12px' }}>Name</Text>
+                        <span>{c.metadata?.name}</span>
+                      </Box>
+                      <Box>
+                        <Text style={{ fontWeight: 600, marginBottom: '12px' }}>Start time</Text>
+                        <span>{c.startTime.toDateString()}</span>
+                      </Box>
+                      <Box>
+                        <Text style={{ fontWeight: 600 }}>Num to drop</Text>
+                        <span>{c.availableSupply}</span>
+                      </Box>
+                      <Box>
+                        <Text style={{ fontWeight: 600 }}>Price</Text>
+                        {parseCurrencyDecimals(c.price as any, c.currencyMetadata.decimals)} <span>{c.currencyMetadata.symbol}</span>
+                      </Box>
+                      <Box>
+                        <Text style={{ fontWeight: 600 }}>Limit per wallet</Text>
+                        <span>{c.maxClaimablePerWallet}</span>
+                      </Box>
+                    </HStack>
                   </Box>
-                  <Box>
-                    <Text style={{ fontWeight: 600 }}>Num to drop</Text>
-                    <span>{c.availableSupply}</span>
-                  </Box>
-                  <Box>
-                    <Text style={{ fontWeight: 600 }}>Price</Text>
-                    {parseCurrencyDecimals(c.price as any, c.currencyMetadata.decimals)} <span>{c.currencyMetadata.symbol}</span>
-                  </Box>
-                  <Box>
-                    <Text style={{ fontWeight: 600 }}>Limit per wallet</Text>
-                    <span>{c.maxClaimablePerWallet}</span>
-                  </Box>
-                </HStack>
+                )}
               </Box>
-            )}
+            ))}
+          </>
+        ) : (
+          <VStack spacing={8}>
+            <Alert
+              status="error"
+              variant="subtle"
+              flexDirection="column"
+              alignItems="center"
+              justifyContent="center"
+              textAlign="center"
+              height="200px"
+              borderRadius={4}>
+              <AlertIcon boxSize="40px" mr={0} />
+              <AlertTitle mt={4} mb={1} fontSize="lg">
+                Claim Conditions not set
+              </AlertTitle>
+              <AlertDescription maxWidth="sm">You need to set at least one claim condition to enable persons to claim this nft.</AlertDescription>
+            </Alert>
 
-            {canEditClaim && (
-              <EditClaimConditions
-                nftContract={props.nftContract}
-                nft={props.nft}
-                handleClaimConditionUpdateById={handleClaimConditionUpdateById}
-                claimCondition={props.claimConditions[i]}
-              />
-            )}
-          </Box>
-        ))}
-    </VStack>
+            <Button
+              variant="outline"
+              fontSize={12}
+              colorScheme={props.addClaimPhase ? 'red' : ''}
+              leftIcon={!props.addClaimPhase ? <AddIcon fontSize={10} /> : <CloseIcon fontSize={10} />}
+              onClick={() => props.setAddClaimPhase(!props.addClaimPhase)}>
+              {!props.addClaimPhase ? 'Add Claim Phase' : 'Cancel'}
+            </Button>
+          </VStack>
+        )}
+
+        <AddClaimPhaseButton
+          styles={{ marginTop: '20px', marginBottom: '12px' }}
+          addClaimPhase={props.addClaimPhase}
+          children={!props.addClaimPhase ? 'Add Claim Phase' : 'Cancel'}
+          setAddClaimPhase={props.setAddClaimPhase}
+        />
+      </VStack>
+    </>
   )
 }
+
+type AddClaimPhaseButtonProps = {
+  addClaimPhase: boolean
+  setAddClaimPhase: (args: boolean) => void
+  children: string
+  styles?: React.CSSProperties
+}
+
+function AddClaimPhaseButton(props: AddClaimPhaseButtonProps) {
+  return (
+    <Button
+      style={props.styles}
+      variant="outline"
+      fontSize={12}
+      colorScheme={props.addClaimPhase ? 'red' : ''}
+      leftIcon={!props.addClaimPhase ? <AddIcon fontSize={10} /> : <CloseIcon fontSize={10} />}
+      onClick={() => props.setAddClaimPhase(!props.addClaimPhase)}>
+      {props.children}
+    </Button>
+  )
+}
+
+// TODO: Listen to  `updateClaimCondition` event and refetch latest `claimConditions`
